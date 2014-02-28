e31e26dca1d5109a6f01c3933ef8487d0d3ff1cc
Kevin McAlear <kevin.mcalear@gmail.com>
2014-02-28 11:58:34 -0500

    starting redit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Redit::Application.config.secret_key_base = '0241b88e50dbee2481f3592599f37e280bcc6868ce94c1303c5b9f8e9647a47983a686d3d09288973686f91a2a8565720b97401ea16d49b5de7b8c10b08d9aaa'
